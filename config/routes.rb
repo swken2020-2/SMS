de597e3e1873e835bed9b80b650438546c7d0570
n <2011141001r@ed.fuk.kindai.ac.jp>
2020-11-18 06:45:04 +0000

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+  root 'main#index'
   get 'subjects/index'
   get 'subject/index'
-  root 'main#index'
+  
+  get 'login', to: 'login#index'
+  get 'login/login', to: 'login#index'
+  post 'login/login'
+  get 'login/logout'
 end
-=======
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    get 'login', to: 'login#index'
-    get 'login/login', to: 'login#index'
-    post 'login/login'
-    get 'login/logout'
-end
->>>>>>> 624472f196be0e7e0a865b6fb29ed93593eba198
