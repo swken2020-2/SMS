6904f960949e800ed5f1bb49b44e9e2beaf7c81c
EC2 Default User <ec2-user@ip-172-31-62-1.ec2.internal>
2020-11-18 06:18:23 +0000

    メイン画面作成

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module SMS
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,3 +20,5 @@ module SMS
     # the framework and any gems in your application.
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'subjects/index'
+  get 'subject/index'
+  root 'main#index'
 end
