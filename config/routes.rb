a5f104761b244414d08a1b241bcbeec0252d39f2
n <2011141001r@ed.fuk.kindai.ac.jp>
2020-11-18 05:41:28 +0000

    とりあえず

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'login', to: 'login#index'
+    get 'login/login', to: 'login#index'
+    post 'login/login'
+    get 'login/logout'
 end
