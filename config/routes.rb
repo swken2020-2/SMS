457d2e83f27bb188520c98beed1e31a37a126d4b
EC2 Default User <ec2-user@ip-172-31-62-1.ec2.internal>
2020-12-02 01:51:09 +0000

    ログイン途中まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
+
 Rails.application.routes.draw do
   get 'users/new'
-  get 'main/index'
   root 'main#index'
-  get 'subjects/index'
-  get 'subject/index'
-  get 'users/new', to: 'users#new'
-  post 'users', to: 'users#create'
-  get 'login/index'
+
+  resources :subjects, only: [:index, :new, :create, :destroy]
+  resources :courses, only: [:index, :create, :destroy]
+  
+  get 'login', to: 'login#index'
+  get 'login/login', to: 'login#index'
   post 'login/login'
   get 'login/logout'
 end
